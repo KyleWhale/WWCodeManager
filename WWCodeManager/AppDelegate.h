443123e7fe83b300c268fad31525bf5f67f0a005
李雪健 <18519100241@163.com>
2023-01-30 11:36:00 +0800

    init

---
+++ WWCodeManager/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  WWCodeManager
+//
+//  Created by 李雪健 on 2023/1/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ WWCodeManager/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  WWCodeManager
+//
+//  Created by 李雪健 on 2023/1/30.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
